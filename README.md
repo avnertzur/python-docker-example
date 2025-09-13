d1c5338dbbca272b68b9a08e4b1dd15001fe2a4d
Avner Tzur <avner.zur@gmail.com>
2025-09-13 09:57:54 +0300

    files_added (#1)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # python-docker-example
 simple python FAST API app as docker container
+
+## Build the docker image
+```
+docker build -t my-fastapi-app .
+```
+
+## Run the continer
+```
+docker run -d -p 8000:8000 --name my-fast-api my-fastapi-app
+```
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}   
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
